cd78ad060993755d37b7cb771ac9f30185ed4f2d
ESP32 - ESP8266 para Tec.Dig. UA <125473983+jtoobe@users.noreply.github.com>
2025-05-26 19:17:20 -0300

    Add files via upload

---
+++ ESP32_Nokia.ino
@@ -0,0 +1,21 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(18,23,4,15,2);
+
+void setup()   {
+  Serial.begin(115200);
+  display.begin();
+  display.setContrast(50);
+
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(BLACK);
+  display.setCursor(10,10);
+  display.println("Welcome!");
+  display.display();
+}
+void loop(){
+
+}
